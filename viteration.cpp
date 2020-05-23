88936af43ed800deba9f9d8eb7ea75b05d791f10
SpectrumX <wylieza@gmail.com>
2020-05-23 18:57:09 +0200

    Added PI to give the next best action based on learned policy

--- viteration.cpp
+++ viteration.cpp
@@ -1,10 +1,16 @@
 #include <vector>
 #include <iostream>
+#include <algorithm>
+
+//Hyperparams
+double discount_factor = 0.8;
 
 enum state {s1, s2, s3, s4, s5, s6};
 enum action {left, right, up, down};
 
 const std::vector<std::vector<action>> actions = {{right, down},{right, down, left},{},{up, right},{left, up, right},{left, up}};
+
+std::vector<double> V(6, 0); //Create array V all values init to zero
 
 int reward(state s, action a){
     if(s == s2 && a == right)
@@ -43,6 +49,42 @@ state next_state(state s, action a){
     };
 }
 
+double new_V(state s){
+    double highest_reward = 0;
+    double calculated_reward;
+    for (action a : actions[s]){
+        if(highest_reward < (calculated_reward = reward(s, a) + discount_factor*V[next_state(s, a)]))
+            highest_reward = calculated_reward;
+    }
+    return highest_reward;
+}
+
+action PI(state s){
+    action a = actions[s][0];
+    double highest_reward = reward(s, a) + discount_factor*V[next_state(s, a)];
+    double calculated_reward;
+    for (auto act = ++actions[s].begin(); act < actions[s].end(); ++act){
+        if(highest_reward < (calculated_reward = reward(s, *act) + discount_factor*V[next_state(s, *act)]))
+            highest_reward = calculated_reward;
+            a = *act;
+    }
+    return a;
+}
+
 int main(int agrc, char *argv[]){
     std::cout << "Value Iteration\n";
+
+    double delta = 0;
+    double epsilon = 1e-10;
+    double v;
+
+    do {
+        for (int si = s1; si < s6+1; ++si){
+            v = V[si];
+            V[si] = new_V((state) si);
+
+        }
+
+
+    } while (delta > epsilon);
 }
